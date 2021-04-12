f7763b26fb0193a78823b5392ea26a7a43a951e1
Akshay Salunkhe <akshay.salunkhe705@gmail.com>
2021-04-13 02:38:15 +0530

    live

--- resources/views/document_action/document_pending_for_action.blade.php
+++ resources/views/document_action/document_pending_for_action.blade.php
@@ -32,7 +32,7 @@
                 <td>{{ $item->fetchMainDocumentTitle($item->main_document_id) }}</td>
                 <td>{{ $item->fetctSubDocumentTitle($item->sub_document_id) }}</td>
                 <td>
-                    <a style="width: 20%;" href="{{ url('admin/document_pending_for_action/view') }}/{{ $item->id }}" class="btn btn-info">View</a>
+                    <a style="width: 20%;" href="{{ url('hod/document_views/view_form_document') }}/{{ $item->id }}" class="btn btn-info">View</a>
                     <a style="width: 25%;" href="{{ url('admin/document_pending_for_action/approve') }}/{{ $item->id }}" class="btn btn-success">Approve</a>
                     <a style="width: 20%;" href="{{ url('admin/document_pending_for_action/reject') }}/{{ $item->id }}" class="btn btn-warning">Reject</a>
                     <a style="width: 25%;" href="{{ url('admin/document_pending_for_action/remove') }}/{{ $item->id }}" class="btn btn-danger">Remove</a>
