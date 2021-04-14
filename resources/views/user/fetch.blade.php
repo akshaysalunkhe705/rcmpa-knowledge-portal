2540f2e8154283c99b9fab0dfb931361b76038a1
Akshay Salunkhe <akshay.salunkhe705@gmail.com>
2021-04-15 00:10:20 +0530

    live

--- resources/views/user/fetch.blade.php
+++ resources/views/user/fetch.blade.php
@@ -44,12 +44,12 @@ use App\Models\DepartmentModel;
                     <a class="btn btn-danger" href="{{ url('admin/user/remove') }}/{{ $user->id }}">Delete</a>
                 </th>
             </tr>
-            <div class="modal fade" id="set-permission-{{ $user->id }}" tabindex="-1" role="dialog"
-                aria-labelledby="set-permission-{{ $user->id }}" aria-hidden="true">
+            <div class="modal fade" id="change-password-{{ $user->id }}" tabindex="-1" role="dialog"
+                aria-labelledby="change-password-{{ $user->id }}" aria-hidden="true">
                 <div class="modal-dialog modal-xl" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="set-permission-{{ $user->id }}">Modal title</h5>
+                            <h5 class="modal-title" id="change-password-{{ $user->id }}">Modal title</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
