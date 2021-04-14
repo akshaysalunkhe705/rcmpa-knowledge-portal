6285153e12320ecf752fe76cc5f5427985ac5fc1
Akshay Salunkhe <akshay.salunkhe705@gmail.com>
2021-04-14 17:14:38 +0530

    live

--- resources/views/user/detail.blade.php
+++ resources/views/user/detail.blade.php
@@ -227,9 +227,9 @@ use App\Models\UserDocumentPermissionModel;
                     }
 
                     function checked_all(checkbox_id, user_id, department_id, form_id, main_document_id, sub_document_id, action) {
+                        alert($("#"+checkbox_id).val());
                         if($("#"+checkbox_id).prop("checked") == true)
                         {
-                        alert($("#"+checkbox_id).val());
                             $("#curd-" + checkbox_id).attr('checked', true);
                             $("#view_active-" + checkbox_id).attr('checked', true);
                             $("#view_rejected-" + checkbox_id).attr('checked', true);
