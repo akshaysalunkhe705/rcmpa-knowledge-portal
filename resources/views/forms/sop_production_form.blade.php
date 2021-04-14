ed993f4ac2e3d6ddc5944e2a8a5b80ac36931fb0
Akshay Salunkhe <akshay.salunkhe705@gmail.com>
2021-04-15 04:15:42 +0530

    live

--- resources/views/forms/sop_production_form.blade.php
+++ resources/views/forms/sop_production_form.blade.php
@@ -37,10 +37,10 @@
                     @for ($i = 0; $i < count($documentData['chemical_required']['checmical_name']); $i++)
                         <tr>
                             <td><input class="form-control" type="text" name="chemical_name[]" id="chemical_name" value="{{ isset($documentData['chemical_required']['checmical_name'][$i]) ? $documentData['chemical_required']['checmical_name'][$i] : '' }}"></td>
-                            <td><input class="form-control" type="text" name="chemical_make[]" id="make"  value="{{ isset($documentData['chemical_required']['make'][$i]) ? $documentData['chemical_required']['make'][$i] : '' }}"></td>
-                            <td><input class="form-control" type="text" name="chemical_grade_purity[]" id="grade_purity" value="{{ isset($documentData['chemical_required']['grade_purity'][$i]) ? $documentData['chemical_required']['grade_purity'][$i] : '' }}"></td>
-                            <td><input class="form-control" type="text" name="chemical_quantity[]" id="quantity" value="{{ isset($documentData['chemical_required']['quantity'][$i]) ? $documentData['chemical_required']['quantity'][$i] : '' }}"></td>
-                            <td><input class="form-control" type="text" name="chemical_unit[]" id="unit" value="{{ isset($documentData['chemical_required']['unit'][$i]) ? $documentData['chemical_required']['unit'][$i] : '' }}"></td>
+                            <td><input class="form-control" type="text" name="chemical_make[]" id="make"  value="{{ isset($documentData['chemical_required']['chemical_make'][$i]) ? $documentData['chemical_required']['chemical_make'][$i] : '' }}"></td>
+                            <td><input class="form-control" type="text" name="chemical_grade_purity[]" id="grade_purity" value="{{ isset($documentData['chemical_required']['chemical_grade_purity'][$i]) ? $documentData['chemical_required']['chemical_grade_purity'][$i] : '' }}"></td>
+                            <td><input class="form-control" type="text" name="chemical_quantity[]" id="quantity" value="{{ isset($documentData['chemical_required']['chemical_quantity'][$i]) ? $documentData['chemical_required']['chemical_quantity'][$i] : '' }}"></td>
+                            <td><input class="form-control" type="text" name="chemical_unit[]" id="unit" value="{{ isset($documentData['chemical_required']['chemical_unit'][$i]) ? $documentData['chemical_required']['chemical_unit'][$i] : '' }}"></td>
                         </tr>
                     @endfor
                 @endif
