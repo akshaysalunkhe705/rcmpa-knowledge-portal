1a8897f4c1f8135c30acbae4db16c9c294f39184
Akshay Salunkhe <akshay.salunkhe705@gmail.com>
2021-04-15 08:55:44 +0530

    live

--- app/Models/DocumentsModel.php
+++ app/Models/DocumentsModel.php
@@ -169,7 +169,7 @@ class DocumentsModel extends Model implements BaseModelInterface
     ///
     public function getLatestVersion($sub_document_id)
     {
-        $documentVersionNumber = DocumentsModel::select('version_number')->where('sub_document_id', $sub_document_id)->where('status', 'ACTIVE')->max('version_number');
+        $documentVersionNumber = DocumentsModel::select('version_number')->where('sub_document_id', $sub_document_id)->max('version_number');
         return floatval($documentVersionNumber) + floatval(0.1);
     }
     public function archivedPreviousVersion($sub_document_id)
