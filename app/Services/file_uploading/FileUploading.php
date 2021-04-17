cb90b0837051966432689f30fb052b9f593d9ae2
Akshay Salunkhe <akshay.salunkhe705@gmail.com>
2021-04-17 07:43:06 +0530

    live

--- app/Services/file_uploading/FileUploading.php
+++ app/Services/file_uploading/FileUploading.php
@@ -6,37 +6,37 @@ use App\Services\directory_service\DirectoryService;
 
 class FileUploading
 {
-    public $request, $attribute_name, $id, $path, $validations;
+    public $request, $file, $id, $path, $validations;
     public function uploadFile()
     {
-        if ($this->attribute_name != null) {
+        if ($this->file != null) {
             $dirModel = new DirectoryService();
             $dirModel->createDir($this->path);
 
-            if ((empty($this->request)) && (empty($this->path)) && (empty($this->attribute_name))) {
+            if ((empty($this->request)) && (empty($this->path)) && (empty($this->file))) {
                 return "Request And File And Path Can Not Be EMPTY";
             }
             if (!empty($this->validations)) {
                 $this->request->validate([
-                    $this->attribute_name => $this->validations,
+                    $this->file => $this->validations,
                 ]);
             }
 
-            $imageName = time().rand(1,100) . '.' . $this->attribute_name->extension();
+            $imageName = time().rand(1,100) . '.' . $this->file->extension();
             if (!is_dir(public_path($this->path))) {
                 mkdir(public_path($this->path));
             }
-            $this->attribute_name->move(public_path($this->path) . '/', $imageName);
+            $this->file->move(public_path($this->path) . '/', $imageName);
             return $this->path . '/' . $imageName;
         }
     }
 
-    public function UploadFileAndUpdateInDB($ModelClass, $imageNamePath)
-    {
-        if ($this->request->{$this->attribute_name} != null) {
-            $model = $ModelClass::find($this->id);
-            $model->{$this->attribute_name} = $imageNamePath;
-            $model->save();
-        }
-    }
+    // public function UploadFileAndUpdateInDB($ModelClass, $imageNamePath)
+    // {
+    //     if ($this->request->{$this->file} != null) {
+    //         $model = $ModelClass::find($this->id);
+    //         $model->{$this->file} = $imageNamePath;
+    //         $model->save();
+    //     }
+    // }
 }
