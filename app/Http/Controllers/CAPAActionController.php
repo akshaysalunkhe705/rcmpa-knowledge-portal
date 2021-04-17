ea4409496110a0bf877878af4bd2febe39c716f3
Akshay Salunkhe <akshay.salunkhe705@gmail.com>
2021-04-17 06:38:32 +0530

    live

--- app/Http/Controllers/CAPAActionController.php
+++ app/Http/Controllers/CAPAActionController.php
@@ -19,7 +19,7 @@ class CAPAActionController extends Controller
     //---------------------CREATE AND UPDATE
     public function process_and_flow_control(Request $request)
     {
-        return $request->file('process_reference_document_urls');
+        return $request;
         $imagePath = array();
         $fileUploading = new FileUploading();
         foreach ($request->process_reference_document_urls as $file) {
